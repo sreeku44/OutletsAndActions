563d663a998caab098f9ca4a835d70940f1d6cee
sreeku44 <2sreeku44@gmail.com>
2017-01-30 13:07:48 -0600

    Initial Commit

---
+++ OutletsAndActions/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OutletsAndActions
+//
+//  Created by Sreekala Santhakumari on 1/30/17.
+//  Copyright © 2017 Klas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
